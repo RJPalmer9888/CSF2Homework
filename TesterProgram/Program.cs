080f75f82f326298026505c0ea6fbaf35d760339
Ryan Palmer <rjpalmer9888@gmail.com>
2021-05-26 12:44:27 -0500

    Student

--- TesterProgram/Program.cs
+++ TesterProgram/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassesLibrary;
 
 namespace TesterProgram
 {
@@ -10,7 +11,8 @@ namespace TesterProgram
     {
         static void Main(string[] args)
         {
-
+            Student s1 = new Student("Ryan","Palmer","54092401",3.6f);
+            Console.WriteLine(s1);
         }
     }
 }

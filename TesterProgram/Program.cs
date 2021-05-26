13ddb45b06d12924464db5ffc8408224131357c0
Ryan Palmer <rjpalmer9888@gmail.com>
2021-05-26 12:52:07 -0500

    Vehicle

--- TesterProgram/Program.cs
+++ TesterProgram/Program.cs
@@ -13,6 +13,9 @@ namespace TesterProgram
         {
             Student s1 = new Student("Ryan","Palmer","54092401",3.6f);
             Console.WriteLine(s1);
+
+            Vehicle v1 = new Vehicle("Nissan", "Sentra", 2016, 402.4f);
+            Console.WriteLine(v1);
         }
     }
 }
